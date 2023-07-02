83c2d89e941a79527c87a5c7f486d15473f21171
sahil <91758633+slordsahil@users.noreply.github.com>
2023-07-03 04:01:36 +0530

    Add files via upload

---
+++ sari kainat lag jati hai/unique_char.py
@@ -0,0 +1,16 @@
+#%%
+def unique_char(s):
+    return len(set(s))==len(s)
+    # unique_char=[]
+    # for i in s:
+    #     if i in unique_char:
+    #         return False
+    #     else:
+    #         unique_char.append(i)
+    # return True
+
+unique_char=unique_char('sahil tkr')
+print(unique_char)
+
+    
+# %%
