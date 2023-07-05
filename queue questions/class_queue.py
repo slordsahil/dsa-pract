87b13f061fcba7666ea5d46ac3c0c99a2e636650
slordsahil <91758633+slordsahil@users.noreply.github.com>
2023-07-05 23:38:12 +0530

    by sahil

---
+++ queue questions/class_queue.py
@@ -0,0 +1,17 @@
+class queue(object):
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items==[]
+    
+    def enqueue(self,item):
+        self.items.insert(0,item)
+        
+    def dequeue(self):
+        self.items.pop()
+        
+    def size(self):
+        return len(self.items)
+    
+
