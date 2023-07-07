c89b31f86a4a468547075275f7eccef92b9e68db
slordsahil <91758633+slordsahil@users.noreply.github.com>
2023-07-08 00:37:59 +0530

    by sahil

---
+++ queue questions/implement_queue_2stack.py
@@ -0,0 +1,20 @@
+class queue_2stack():
+    def __init__(self):
+        self.stack1=[]
+        self.stack2=[]
+        
+    def enqueue(self,item):
+        self.stack1.append(item)
+        
+    def dequeue(self):
+        if self.stack2==[]:
+            while self.stack1!=[]:
+                self.stack2.append(self.stack1.pop())
+        else:
+            return self.stack2.pop()
+        return self.stack2.pop()
+
+
+
+
+    
