06a2543b7fff0c83cdbf9b3a9f0394260bc67882
slordsahil <91758633+slordsahil@users.noreply.github.com>
2023-07-09 01:15:07 +0530

    by sahil

---
+++ linked list questions/cycle_check_single_linked_list.py
@@ -0,0 +1,29 @@
+class node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+    def next(self):
+        return self.next
+        
+class check_cycle(node):
+    
+    def check(sol):
+        node_list=[]
+        while node.next(sol)!=None:
+            if sol not in node_list:
+                node_list.append(sol)
+                sol = node.next(sol)
+            else:
+                return str(True )+str("its cycle")
+        return str(False )+str("its not cycle")
+     
+
+a=node(input)
+b=node(2)
+c=node(3)
+d=node(4)
+a.next=b
+b.next=c
+c.next=d
+test=check_cycle.check(a)
+print(test)          
