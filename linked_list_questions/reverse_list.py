df4f2eff8ef94244fa05fe49e8c4ff12e07db7d7
slordsahil <91758633+slordsahil@users.noreply.github.com>
2023-07-11 04:34:30 +0530

    by sahil in amravati created a file which has the code of reversing the value of nodes

---
+++ linked_list_questions/reverse_list.py
@@ -0,0 +1,17 @@
+class node():
+    def __init__(self,value):
+        self.value=value
+        self.nextnode=None
+        
+        
+    def reverse(N):
+        total=[N]
+        while N.nextnode!=None:
+            N=N.nextnode
+            total.append(N)
+            print("hi")
+        for i in range(len(total)//2):
+            total[i].value,total[-(i+1)].value=total[-(i+1)].value,total[i].value
+
+        
+            
