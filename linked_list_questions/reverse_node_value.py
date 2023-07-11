634d51b11a675d0d2bb9c85c4382254219f2a765
slordsahil <91758633+slordsahil@users.noreply.github.com>
2023-07-11 15:54:09 +0530

    by sahil

--- linked_list_questions/reverse_node_value.py
+++ linked_list_questions/reverse_node_value.py
@@ -9,7 +9,6 @@ class node():
         while N.nextnode!=None:
             N=N.nextnode
             total.append(N)
-            print("hi")
         for i in range(len(total)//2): 
             total[i].value,total[-(i+1)].value=total[-(i+1)].value,total[i].value
 
