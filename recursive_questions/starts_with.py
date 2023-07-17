7242223ea8206a96a7fd10b5ba918c6336f6d52f
slordsahil <91758633+slordsahil@users.noreply.github.com>
2023-07-18 00:42:09 +0530

    by sahil

---
+++ recursive_questions/starts_with.py
@@ -0,0 +1,13 @@
+#%%
+def starts_with(string,list_words,other=None):
+    if other==None:
+        other = []
+    
+    for word in list_words:
+        if string.startswith(word):
+            other.append(word)
+            return starts_with(string[len(word):],list_words,other)
+    return other
+
+print(starts_with('iamsahilkatkar',['sahil','katkar','i','am']))
+# %%
