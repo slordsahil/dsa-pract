c722b332d57528b9e29f837777144b1538fe415b
sahil katkar <slordsahil>
2023-10-02 23:02:16 +0530

    ff

--- binary search.py
+++ binary search.py
@@ -1,14 +1,16 @@
+#%%
 def bs(k,arr):
     mid=len(arr)//2
     if arr[mid]==k:
-        return mid
+        return arr[mid]
     elif k>arr[mid]:
-        arr=arr[mid:]
-        return bs(k,arr)
+        arr1=arr[mid:]
+        return bs(k,arr1)
     else:
-        arr=arr[:mid+1]
+        arr1=arr[:mid+1]
         print(arr,mid+1)
-        return bs(k,arr)
+        return bs(k,arr1)
         
         
 bs(4,[1,2,3,4,5])
+# %%
